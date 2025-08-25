70736196f5cd5e6b2b492bfb68d831b9dd4a152b
ishangupta-webengage <ishan.gupta@webengage.com>
2025-08-25 16:01:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,20 @@ import App from './App';
 
 const userID = 'user' + Math.floor(Math.random() * 1000);
 
-webengage.user.setAttribute('we_email', userID + '@email.com');
-webengage.user.setAttribute('we_birth_date', '1998-02-14');
-webengage.user.setAttribute('we_phone', '+917509309539');
-webengage.user.setAttribute('we_gender', 'male');
-webengage.user.setAttribute('we_first_name', 'John' + Math.floor(Math.random() * 1000));
-webengage.user.setAttribute('we_last_name', 'Doe' + Math.floor(Math.random() * 1000));
-webengage.user.setAttribute('we_company', 'swanishan');
-webengage.user.setAttribute('we_email_opt_in', true); //Email
-webengage.user.setAttribute('we_sms_opt_in', true); //SMS
-webengage.user.setAttribute('we_whatsapp_opt_in', true);
-
-webengage.user.login(userID);
+webengage.onReady(function () {
+	webengage.user.setAttribute('we_email', userID + '@email.com');
+	webengage.user.setAttribute('we_birth_date', '1998-02-14');
+	webengage.user.setAttribute('we_phone', '+917509309539');
+	webengage.user.setAttribute('we_gender', 'male');
+	webengage.user.setAttribute('we_first_name', 'John' + Math.floor(Math.random() * 1000));
+	webengage.user.setAttribute('we_last_name', 'Doe' + Math.floor(Math.random() * 1000));
+	webengage.user.setAttribute('we_company', 'swanishan');
+	webengage.user.setAttribute('we_email_opt_in', true); //Email
+	webengage.user.setAttribute('we_sms_opt_in', true); //SMS
+	webengage.user.setAttribute('we_whatsapp_opt_in', true);
+	
+	webengage.user.login(userID);
+});
 
 window.addEventListener('beforeunload', function (event) {
     webengage.user.logout();
